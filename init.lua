3cc70d1a7f90e6e937d63e8fd9002e59579c2d85
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2025-10-19 12:06:21 +0200

    reference the fork repository

--- init.lua
+++ init.lua
@@ -16,7 +16,7 @@ local lazy_config = require "configs.lazy"
 -- load plugins
 require("lazy").setup({
   {
-    "NvChad/NvChad",
+    "yacosta738/NvChad",
     lazy = false,
     branch = "v2.5",
     import = "nvchad.plugins",
